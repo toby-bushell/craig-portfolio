a2e5b905e03c2b6d1f0d479beefa13ce1deae21a
Toby Bushell <toby.bushell@manifesto.co.uk>
2017-06-26 23:00:26 +0100

    browser sync removed

--- footer.php
+++ footer.php
@@ -22,8 +22,6 @@
   </footer>
   </div>
   <?php wp_footer(); ?>
-  <script id="__bs_script__">//<![CDATA[
-    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.11.2.js'><\/script>".replace("HOST", location.hostname));
-//]]></script>
+
   </body>
 </html>
